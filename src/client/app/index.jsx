1457c62a681f4e334da771beb7ea79f724fb2d85
danillopneto <daynilou@gmail.com>
2017-10-28 15:59:42 -0200

    Inclusão de barra de menu

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Select from 'react-select';
+import {Nav, Navbar, NavItem, MenuItem, NavDropdown} from 'react-bootstrap';
 
 class Banner extends React.Component {
     render(){
         return <img src="./resources/img/banner.jpg" width="100%" />
     }
+}
+
+class MenuPrincipal extends React.Component {
+  render() {
+    return <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="./index.html">Minha Saúde</a>
+        </Navbar.Brand>
+      </Navbar.Header>
+      <Nav>
+        <NavItem eventKey={1} href="#">Identificadores</NavItem>
+        <NavItem eventKey={2} href="#">Nomes</NavItem>
+        <NavItem eventKey={3} href="#">Dados demográficos</NavItem>
+        <NavItem eventKey={4} href="#">Endereços</NavItem>
+      </Nav>
+    </Navbar>
+  }
 }
 
 const ufs = [
@@ -39,3 +58,4 @@ const ufs = [
 ];
 
 render(<Banner/>, document.getElementsByTagName('header')[0]);
+render(<MenuPrincipal/>, document.getElementById('menuPrincipal'));
