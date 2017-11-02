f5cc9cc26922b9d9f6a1f0a6f14f3430b10a96b8
danillopneto <daynilou@gmail.com>
2017-11-02 18:37:37 -0200

    Criação de jsx específico para identificadores

---
+++ src/client/app/identificadores.jsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Select from 'react-select';
+import {Col, Button, ControlLabel, Form, FormControl, FormGroup, MenuItem, Nav, Navbar, NavItem, NavDropdown, Panel} from 'react-bootstrap';
+
+export class DadosGeraisDoIdentificador extends React.Component {
+    render() {
+      return <Panel>
+          <FormGroup controlId="TipoDoIdentificador" className="col-sm-6">      
+              <Col sm={12}>
+                <label>Tipo do identificador</label>
+              </Col>
+              <Col sm={12}>
+                <FormControl componentClass="select" placeholder="select">
+                  <option value="01">Único dentro da organização</option>
+                  <option value="02">Número do prontuário</option>
+                  <option value="21">Radiologia</option>
+                  <option value="22">Patologia</option>
+                  <option value="23">Farmácia</option>
+                </FormControl>
+              </Col>
+          </FormGroup>
+          <FormGroup controlId="AreaGeografica" className="col-sm-6">
+            <Col sm={12}>
+              <ControlLabel>Área geográfica</ControlLabel>      
+            </Col>
+            <Col sm={12}>
+              <FormControl componentClass="select" placeholder="select">
+                <option value="1">Local</option>
+                <option value="2">Área/região/distrito</option>
+                <option value="3">Estado/província/território</option>
+                <option value="4">Nacional</option>
+              </FormControl>
+            </Col>
+          </FormGroup>
+          <FormGroup controlId="Designacao" className="col-sm-6">
+            <Col sm={12}>
+              <ControlLabel>Designação</ControlLabel>      
+            </Col>
+            <Col sm={12}>
+              <FormControl componentClass="input" maxLength="25" />
+            </Col>
+          </FormGroup>
+          <FormGroup controlId="DataIdentificador" className="col-sm-6">
+            <Col sm={12}>
+              <ControlLabel>Data</ControlLabel>      
+            </Col>
+            <Col sm={12}>
+              <FormControl componentClass="input" type="date" />
+            </Col>
+          </FormGroup>
+          <FormGroup controlId="Emissor" className="col-sm-6">
+            <Col sm={12}>
+              <ControlLabel>Emissor</ControlLabel>      
+            </Col>
+            <Col sm={12}>
+              <FormControl componentClass="input" maxLength="25" />
+            </Col>
+          </FormGroup>
+        </Panel>
+    }
+  };
+  
+export class Certidao extends React.Component {
+    render() {
+    return <Panel>
+        <FormGroup controlId="TipoDeCertidao" className="col-sm-12">      
+            <Col sm={12}>
+            <label>Tipo de certidão</label>
+            </Col>
+            <Col sm={12}>
+            <FormControl componentClass="select" placeholder="select">
+                <option value="nascimento">Nascimento</option>
+                <option value="casamento">Casamento</option>
+                <option value="divorcio">Divórcio</option>
+            </FormControl>
+            </Col>
+        </FormGroup>
+        <FormGroup controlId="NomeDoCartorio" className="col-sm-12">
+            <Col sm={12}>
+            <ControlLabel>Nome do Cartório</ControlLabel>      
+            </Col>
+            <Col sm={12}>
+            <FormControl componentClass="input" maxLength="25" />
+            </Col>
+        </FormGroup>
+        <FormGroup controlId="LivroDoCartorio" className="col-sm-4">
+            <Col sm={12}>
+            <ControlLabel>Livro</ControlLabel>      
+            </Col>
+            <Col sm={12}>
+            <FormControl componentClass="input" type="number" />
+            </Col>
+        </FormGroup>
+        <FormGroup controlId="FolhaDoCartorio" className="col-sm-4">
+            <Col sm={12}>
+            <ControlLabel>Folha</ControlLabel>      
+            </Col>
+            <Col sm={12}>
+            <FormControl componentClass="input" type="number" />
+            </Col>
+        </FormGroup>
+        <FormGroup controlId="TermoDoCartorio" className="col-sm-4">
+            <Col sm={12}>
+            <ControlLabel>Termo</ControlLabel>      
+            </Col>
+            <Col sm={12}>
+            <FormControl componentClass="input" type="number" />
+            </Col>
+        </FormGroup>
+        </Panel>
+    }
+};
